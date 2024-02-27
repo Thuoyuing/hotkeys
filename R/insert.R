061d5c6aa20ddb43d4b841d7b1f3383540c97ed6
Guohuan Zhang (张国欢) <105842911+Thuoyuing@users.noreply.github.com>
2024-02-27 15:19:14 +0800

    Update insert.R

--- R/insert.R
+++ R/insert.R
@@ -14,21 +14,21 @@ a2 <- function() {
 
 #' Insert \%>>\%.
 #' @export
-a2 <- function() {
+a3 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " %>>% ")
 }
 
 #' Insert \%T>\%.
 #' @export
-a3 <- function() {
+a4 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " %T>% ")
 }
 
 #' Insert <-.
 #' @export
-a4 <- function() {
+a5 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " <- ")
 }
@@ -36,35 +36,35 @@ a4 <- function() {
 
 #' Insert \%==\% .
 #' @export
-a5 <- function() {
+a6 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " %==% ")
 }
 
 #' Insert \%$\% .
 #' @export
-a6 <- function() {
+a7 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " %$% ")
 }
 
 #' Insert \%\% .
 #' @export
-a7 <- function() {
+a8 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " %% ")
 }
 
 #' Insert \%\/\% .
 #' @export
-a8 <- function() {
+a9 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " %/% ")
 }
 
 #' Insert -> .
 #' @export
-a9 <- function() {
+a10 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " -> ")
 }
@@ -72,34 +72,34 @@ a9 <- function() {
 
 #' Insert \%+replace\% .
 #' @export
-a10 <- function() {
+a11 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " %+replace% ")
 }
 
 #' Insert \%w/o\% .
 #' @export
-a11 <- function() {
+a12 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " %w/o% ")
 }
 
 #' Insert \%*\% .
 #' @export
-a12 <- function() {
+a13 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " %*% ")
 }
 
 #' Insert \%<>\% .
 #' @export
-a13 <- function() {
+a14 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " %<>% ")
 }
 #' Insert |> .
 #' @export
-a14 <- function() {
+a15 <- function() {
   loc <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range
   rstudioapi::insertText(loc, " |> ")
 }
